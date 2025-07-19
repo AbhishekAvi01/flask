1ed07f8ab6d7b8b8bda6b22a7764b717c2501f32
Abhishek Kumar <148855924+AbhishekAvi01@users.noreply.github.com>
2025-07-19 18:39:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask in Docker via Jenkins!"
+    return "Welcome Page - This is the main home page"
+
+@app.route('/header')
+def header():
+    return "This is the HEADER with logo and nav bar"
+
+@app.route('/body')
+def body():
+    return "This is the BODY section with main content of the website"
+
+@app.route('/footer')
+def footer():
+    return "Footer - All rights reserved"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
